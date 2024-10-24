568d72fa00b39e9645a1efeefeadd9746b80caf0
Urkel <urkelcodes7@gmail.com>
2024-10-24 22:03:39 +0100

    new commit

--- app/_components/search/Search.tsx
+++ app/_components/search/Search.tsx
@@ -6,17 +6,20 @@ import { useSearchQuery } from '@/app/_dataOperations/queries/queries';
 import { useSearchParams } from 'next/navigation';
 import SearchBox from './SearchBox';
 import NoDataFound from '../NoDataFound';
+import { Suspense } from 'react';
 
 const Search = () => {
 	const searchParams = useSearchParams()
 	const query = searchParams.get("query") as string
 	const { data } = useSearchQuery(query)
-    return (
-    	<div className="flex flex-col items-center mt-5 gap-10">
-	      <SearchBox />
-	      {data?.length === 0 ? <NoDataFound  information='No Data Found' /> : <Tests title='Search Result' tests={data} /> }
-	    </div>
-    );
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<div className="flex flex-col items-center mt-5 gap-10">
+				<SearchBox />
+				{data?.length === 0 ? <NoDataFound information='No Data Found' /> : <Tests title='Search Result' tests={data} />}
+			</div>
+		</Suspense>
+	);
 };
 
 export default Search;
