22beefa093fd2c0c417ac6912605833a94cdd709
Urkel <urkelcodes7@gmail.com>
2024-12-05 01:00:42 +0100

    new channges

--- app/_components/UserDropdown.tsx
+++ app/_components/UserDropdown.tsx
@@ -14,14 +14,46 @@ import { Brain, Settings, User, UserCircle } from "lucide-react";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import LogoutButton from "./LogoutButton"
 import Link from "next/link"
+import { useRouter } from "next/navigation";
+
+
 
 const UserDropdown = () => {
   const { user } = useUserStore();
+  const router = useRouter()
   const [isActive, setIsActive] = useState(false);
-  const handleToggle = ()=>{
+  const handleToggle = () => {
     setIsActive(!isActive);
     alert(121);  // remove this line for actual use
     console.log(88888)
+  }
+
+  const items = [
+    {
+      name: "Profile",
+      icon: <User />,
+      link: `/profile/${user.id}`,
+    },
+    {
+      name: "Attempted Tests",
+      icon: <Brain />,
+      link: "/attempted-tests"
+    },
+    {
+      name: "My Tests",
+      icon: <Brain />,
+      link: "/my-tests?filter=public"
+    },
+    {
+      name: "Settings",
+      icon: <Settings />,
+      link: "/"
+    },
+  
+  ]
+
+  const navigate = (link: string) => {
+    router.push(link)
   }
   return (
     <div>
@@ -34,9 +66,8 @@ const UserDropdown = () => {
             <UserCircle className="text-zinc-600" />
             {/* {user.username} */}
             <FaAngleDown
-              className={`text-zinc-500 w-5 h-5 ${
-                isActive ? "rotate-0" : "" // rotate dropdown menu icon when it is open
-              }`}
+              className={`text-zinc-500 w-5 h-5 ${isActive ? "rotate-0" : "" // rotate dropdown menu icon when it is open
+                }`}
             />
           </div>
           {/* <FaAngleUp className="text-zinc-500 w-5 h-5" /> */}
@@ -44,30 +75,13 @@ const UserDropdown = () => {
         <DropdownMenuContent className=" z-[9999]">
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Link href={`/profile/${user.id}`} className="flex gap-1 items-center">
-                <User className="text-zinc-800 w-4 h-4" />
-                Profile
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href="/attempted-tests" className="flex gap-1 items-center">
-                <Brain className="text-zinc-800 w-4 h-4" />
-                Attempted Tests
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href="/my-tests?filter=public" className="flex gap-1 items-center">
-                <Brain className="text-zinc-800 w-4 h-4" />
-                My Tests
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href="#" className="flex gap-1 items-center">
-                <Settings className="text-zinc-800 w-4 h-4" />
-                Settings
-            </Link>
-          </DropdownMenuItem>
+          {items.map((item, index) => (
+            <DropdownMenuItem onClick={() => navigate(item.link)} key={index}>
+             {item.icon}
+             {item.name}
+            </DropdownMenuItem>
+          ))}
+
           <DropdownMenuSeparator />
           <DropdownMenuItem>
             <LogoutButton />
