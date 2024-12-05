a663fa2b39d207d70d6c6275d266e65cde683755
Urkel <urkelcodes7@gmail.com>
2024-12-05 01:33:29 +0100

    new channges

--- app/_components/Home/Banner1.tsx
+++ app/_components/Home/Banner1.tsx
@@ -5,19 +5,19 @@ import Link from "next/link";
 const Banner1 = () => {
   return (
     <div className="bg-neutral-950 min-h-56 text-white mt-20 p-12 md:p-20 flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-14">
         <h1 className="md:text-7xl text-3xl max-w-[60%] font-bold font-cinzel">
           Certification Made <span className="text-primary">Easy...</span>
         </h1>
         <h1 className="md:text-4xl text-xl font-semibold font-cinzel">
           With Skill<span className="text-primary">Cert</span>
         </h1>
-        <Link href="/auth/register" className="border-2 border-primary rounded-md text-white text-[16px] px-6 py-[8px] w-fit hover:bg-primary transition">Register Now</Link>
+        {/* <Link href="/auth/register" className="border-2 border-primary rounded-md text-white text-[16px] px-6 py-[8px] w-fit hover:bg-primary transition">Register Now</Link> */}
       </div>
 
-      {/* <div className="animate-bounce">
+      <div className="animate-bounce">
         <File width={150} height={150}/>
-      </div> */}
+      </div>
     </div>
   );
 };
