b00acd1a839e7707f89097fe7ec6fed9e9321bfb
Urkel <urkelcodes7@gmail.com>
2024-12-18 15:35:21 +0100

    made new changes

--- app/(routes)/coming-soon/page.tsx
+++ app/(routes)/coming-soon/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div className=' flex items-center justify-center gap-5 pt-20'>
+        <div className=' flex flex-col items-center justify-center gap-5 pt-20'>
             <Wrench className='w-20 h-20 text-gray-400' />
             <h1 className='text-xl sm:text-3xl font-bold leading-snug text-center bg-gradient-to-r from-blue-800 via-gray-800 to-blue-500 bg-clip-text text-transparent'>
             	THIS FEATURE IS YET TO BE IMPLEMENTED
