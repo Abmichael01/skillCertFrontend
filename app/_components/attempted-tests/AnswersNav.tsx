2319c2f242b2f4658fbd3c2c04fdb22630fdd9bf
Urkel <urkelcodes7@gmail.com>
2024-12-18 16:52:34 +0100

    made new changes

--- app/_components/attempted-tests/AnswersNav.tsx
+++ app/_components/attempted-tests/AnswersNav.tsx
@@ -24,25 +24,29 @@ const AnswersNav = () => {
   return (
     <div className="border border-zinc-300 h-fit p-5 col-span-2 lg:sticky top-[170px] rounded-md w-full lg:w-[350px] 3xl:w-[500px] z-10">
       <div className="flex flex-wrap gap-3">
-        {questions.map(
+        {questions?.map(
           (question, index) => {
             const userAnswer = attemptAnswers.find(answer => answer.question === question.id) as Answer
             const correctOption = question.options.find(option => option.is_correct) as Option
-            const answeredCorrectly = userAnswer.selected_option === correctOption.id
+            const answeredCorrectly = userAnswer?.selected_option === correctOption?.id
+            const isCurrentQuestion = currentQuestionIndex === index;
               return (
-                  !(index == 0) && (
+                  (
                     <div
                       key={index}
                       onClick={() => {
                         setCurrentQuestionIndex((index));
                       }}
-                      className={`w-10 h-10 flex items-center justify-center text-xl rounded-full border transition cursor-pointer  ${
-                        (currentQuestionIndex === index && answeredCorrectly) ? "bg-emerald-500 text-white hover:bg-emerald-500 border-emerald-500"
-                        : (currentQuestionIndex === index && !answeredCorrectly) ? "bg-rose-500 text-white hover:bg-rose-500 border-rose-500" : ""
-
-                      } `}
+                      className={`w-10 h-10 flex items-center justify-center text-xl rounded-full border transition cursor-pointer ${ 
+                        isCurrentQuestion 
+                        ? answeredCorrectly 
+                        ? "bg-emerald-500 text-white hover:bg-emerald-500 border-emerald-500" 
+                        : "bg-rose-500 text-white hover:bg-rose-500 border-rose-500" 
+                        : answeredCorrectly 
+                        ? "border-emerald-500" 
+                        : "border-rose-500" }`}
                     >
-                      {index+1}
+                      {index + 1}
                     </div>
                   )
                 )
